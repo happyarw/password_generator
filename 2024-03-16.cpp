f815d12a6ab962250645332b13ddbe7b66bfb2e5
Andreas Wahl <andreas.wahl@hotmail.com>
2024-03-17 11:07:55 +0100

    Added some Comments for easier reading

--- 2024-03-16.cpp
+++ 2024-03-16.cpp
@@ -1,14 +1,11 @@
 #include "pch.h"
 #include <iostream>
-#include <ctime>
+#include <ctime> //including ctime header for the time function
 #include <string>
 using namespace System;
 using namespace std;
 
-void intialize_random() {
-    srand(time(NULL));
-}
-
+//generate 12 random characters or numbers
 string password_gen() {
     string password = "";
     char alphabet[90] = { "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!§$%&/()=?´`+*#'_-.:,;<>^" };
@@ -20,15 +17,19 @@ string password_gen() {
     return password;
 }
 
-
 int main()
 {
-    intialize_random();
+    //seed the random number generator using the current time
+    //so its different random sequences each time the programm runs
+    //this sould be called only once at the beginning of the programm!
+    srand(time(NULL));
 
+    //stores the passwords in strings
     string password1 = password_gen();
     string password2 = password_gen();
     string password3 = password_gen();
 
+    //console output
     cout << password1 << endl;
     cout << password2 << endl;
     cout << password3 << endl;
